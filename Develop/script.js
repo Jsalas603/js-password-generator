9221635d0a4b3bed2fa9a566a28fd5df8779b0f5
jsalas603 <jsalas603@gmail.com>
2021-12-05 19:44:17 -0600

    passworkd generator

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,75 @@
-// Assignment code here
 
+var charString = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklnmopqrstuvwxyz", "0123456789", "~!@#$%^&*()-_=+"];
 
-// Get references to the #generate element
+var userInput = [];
+
+var password = "";
+
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    password = generatePassword();
+    console.log("clicked");
 
-  passwordText.value = password;
+    var passwordText = document.querySelector("#password");
 
+    passwordText.value = password;
+}
+
+function generatePassword() {
+    var characterLength = 0
+    while ((characterLength < 8 || characterLength > 128) || Number.isInteger(characterLength) === false) {
+        characterLength = parseInt(prompt("How many characters would you like your password to be? (8-128)"));
+    }
+
+    var upper = false
+    var lower = false
+    var number = false
+    var symbol = false
+
+    while (!upper && !lower && !number && !symbol) {
+
+        // Uppercase
+        upper = confirm("Click OK to confirm uppercase characters");
+
+        // Lowercase
+        lower = confirm("Click OK to confirm lowercase characters");
+
+        // Numeric 
+        number = confirm("Click OK to confirm numeric characters");
+
+        // Special charcter 
+        symbol = confirm("Click OK to confirm special characters (~!@#$%^&*()-_=+)");
+    }
+
+    if (upper) {
+        userInput.push(charString[0]);
+    }
+
+    if (lower) {
+        userInput.push(charString[1])
+    }
+
+    if (number) {
+        userInput.push(charString[2])
+    }
+
+    if (symbol) {
+        userInput.push(charString[3])
+    }
+
+    var password = "";
+
+    userInput = userInput.join("").split("");
+
+
+    for (var i = 0; i < characterLength; i++) {
+    
+        var index = (Math.floor(Math.random() * userInput.length));
+        password = password + userInput[index]
+    }
+
+    return password
 }
 
 // Add event listener to generate button
